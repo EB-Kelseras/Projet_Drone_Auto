c5868d4e69d989e969270a0811251a1f06ca2a04
EfflammB <efflamm.bodenant@estaca.eu>
2024-10-17 16:44:11 +0200

    Mise en forme

--- Calibrage_camera/Calibrage.py
+++ Calibrage_camera/Calibrage.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 import cv2 as cv
 import glob
@@ -6,21 +7,23 @@ import glob
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
  
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((6*4,3), np.float32)
+objp[:,:2] = np.mgrid[0:6,0:4].T.reshape(-1,2)
  
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
  
-images = glob.glob('*.jpg')
- 
+images = glob.glob('*.png')
+ #%%
 for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
- 
+    cv.imshow('img', gray)
+    cv.waitKey(1000)
+    
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+    ret, corners = cv.findChessboardCorners(gray, (6,4), None)
  
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -30,15 +33,15 @@ for fname in images:
         imgpoints.append(corners2)
  
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv.drawChessboardCorners(img, (6,4), corners2, ret)
         cv.imshow('img', img)
         cv.waitKey(1000)
  
 cv.destroyAllWindows()
-
+#%%
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-img = cv.imread('opencv_frame_24.jpg')
+img = cv.imread('opencv_frame_0.png')
 h,  w = img.shape[:2]
 newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
@@ -58,3 +61,4 @@ for i in range(len(objpoints)):
     mean_error += error
  
 print( "total error: {}".format(mean_error/len(objpoints)) )
+# %%
